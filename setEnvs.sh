91e71f027fd1264f32e1f1757594b3d663737945
kira0826 <curaca492@gmail.com>
2025-02-17 14:36:58 -0500

    hotfix: change mvnw to use mwn image and change user-api url on auth-api

--- setEnvs.sh
+++ setEnvs.sh
@@ -3,4 +3,5 @@
 # Set environment variables
 export JWT_SECRET="PRFT"
 export AUTH_API_PORT="8000"
-export USERS_API_ADDRESS="http://127.0.0.1:8083"
+export USERS_API_ADDRESS="http://users-api:8083"
+export USERS_API_PORT="8083"
